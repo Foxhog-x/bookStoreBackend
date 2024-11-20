cfc96a6944bc8f537df4b30eeb5d059e65c095b9
onkar8 <onkarvgpatil1998@gmail.com>
2024-11-20 23:30:30 +0530

    basic setup and package done

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const PORT = 8000 || process.PORT;
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.send("its working");
+});
+
+app.listen(8000, () => {
+  console.log(`server is listening on port ${PORT}`);
+});
